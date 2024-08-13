037432c2b8508cace78d149e40ac6e56b94f0617
Shivani <unis73376@gmail.com>
2024-08-13 15:47:10 +0530

    Update Interlining.py

--- Interlining.py
+++ Interlining.py
@@ -23,7 +23,7 @@ def is_pure_text_column(series):
     return series.apply(lambda x: isinstance(x, str) and not any(char.isdigit() for char in x)).all()
 
 def main():
-    st.title("LAGUNA INTERFACE")
+    st.title("Sampling Dept.")
 
     # Hide specific Streamlit style elements
     hide_streamlit_style = """
